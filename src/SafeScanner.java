27ae15d0dd6eefc055beb891c0d7c50182aaf2a6
miapanc <115221447+miapanc@users.noreply.github.com>
2022-10-14 13:41:23 +0200

    Update SafeScanner.java

--- src/SafeScanner.java
+++ src/SafeScanner.java
@@ -4,23 +4,4 @@ import java.io.InputStream;
 public class SafeScanner {
     Scanner scan;
     public SafeScanner(InputStream is){
-        this.scan = new Scanner(is);
-    }
-
-    public int getINT(){
-        int entree = this.scan.nextInt();
-        return entree;
-    }
-
-    public int []getDimDepuisFichier(){
-        String entree = this.scan.nextLine();
-        int[] dimension = new int[2];
-        dimension[0] = entree.charAt(0) - '0';
-        dimension[1] = entree.charAt(2) - '0';
-        return dimension;
-    }
-
-    public void fermerScanner(){
-        this.scan.close();
-    }
-}
+        //non finit
